b512ea1da8550b21a8b5f60531cf93fcfa4011db
IURII MIRONOV <yuramiron@yandex-team.ru>
2021-03-19 17:43:23 +0300

    From 1 review

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+let popup = document.querySelector('.popup'); 
+let popupContainer = document.querySelector('.popup__container');
+
+let editButton = document.querySelector('.profile__button_edit'); 
+let closeButton = document.querySelector('.popup__close-button'); 
+
+
+let profileTitle = document.querySelector('.profile__username');
+let profileSubtitle = document.querySelector('.profile__position');
+let popupTitle = document.querySelector('.popup__title');
+let popupSubtitle = document.querySelector('.popup__subtitle');
+
+function openPopup () {
+    popup.classList.toggle('popup_opened');
+    popupTitle.value=profileTitle.textContent;
+    popupSubtitle.value=profileSubtitle.textContent;
+}
+
+function closePopup () {
+    popup.classList.remove('popup_opened');
+}
+
+function submitPopup(evt) {
+    evt.preventDefault()
+    profileTitle.textContent=popupTitle.value;
+    profileSubtitle.textContent=popupSubtitle.value;
+    closePopup();
+}
+
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+popupContainer.addEventListener("submit", submitPopup);
