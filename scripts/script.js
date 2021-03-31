52aba90904696d583a5ad89c9098d409e893408b
IURII MIRONOV <yuramiron@yandex-team.ru>
2021-03-31 12:53:13 +0300

    add card fron JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,42 @@
-let popup = document.querySelector('.popup'); 
+let popup = document.querySelector('.popup');
 let popupContainer = document.querySelector('.popup__container');
 
-let editButton = document.querySelector('.profile__button_type_edit'); 
-let closeButton = document.querySelector('.popup__close-button'); 
-
+let editButton = document.querySelector('.profile__button_type_edit');
+let closeButton = document.querySelector('.popup__close-button');
 
 let profileTitle = document.querySelector('.profile__username');
 let profileSubtitle = document.querySelector('.profile__position');
 let popupTitle = document.querySelector('.popup__input_type_title');
 let popupSubtitle = document.querySelector('.popup__input_type_subtitle');
+
+const templateElements = document.querySelector('.templateElements').content;
+const elements = document.querySelector('.elements');
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
 
 function openPopup () {
     popup.classList.toggle('popup_opened');
@@ -27,7 +55,13 @@ function submitPopup(evt) {
     closePopup();
 }
 
-
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 popupContainer.addEventListener("submit", submitPopup);
+
+initialCards.forEach(function(element){
+    const clonElement = templateElements.querySelector('.element').cloneNode(true);
+    clonElement.querySelector('.element__photo').src = element.link;
+    clonElement.querySelector('.element__text').textContent = element.name;
+    elements.append(clonElement);
+})
